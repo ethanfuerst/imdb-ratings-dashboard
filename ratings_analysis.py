6f92654fb6a06c0117b178f12935187b90bf42c0
Ethan Fuerst <ethanfuerst@gmail.com>
2019-12-19 18:57:59 -0600

    working in twb

--- ratings_analysis.py
+++ ratings_analysis.py
@@ -8,6 +8,21 @@ import matplotlib.pyplot as plt
 with open('ratings.csv', 'r', encoding='mac_roman', newline='') as csvfile:
     df = pd.read_csv(csvfile)
 
+def media_kind(x):
+    if x == 'movie':
+        return 'Movie'
+    elif x == 'tvSeries':
+        return 'TV'
+
+df['Release Date'] = pd.to_datetime(df['Release Date'])
+df['Date Rated'] = pd.to_datetime(df['Date Rated'])
+# Get number of days
+df['Days waited to see'] = df['Date Rated'] - df['Release Date']
+df['Days waited to see'] = (df['Days waited to see'].astype(int) / int(float('8.64e+13'))).astype(int)
+
+df['Title Type'] = df['Title Type'].apply(media_kind)
+
+df.to_csv('ratings_clean.csv', index=False)
 # %%
 plt.scatter(df['IMDb Rating'], df['Your Rating'])
 plt.title('IMDb Rating vs. My Rating')
