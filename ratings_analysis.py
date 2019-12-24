23cc6d29f0dd9f8c4ffb331d164a2ffab8bee68e
Ethan Fuerst <ethanfuerst@gmail.com>
2019-12-23 23:06:51 -0600

    one-hot encoding and checking one-hot columns

--- ratings_analysis.py
+++ ratings_analysis.py
@@ -23,11 +23,41 @@ df['Diff in ratings'] = df['IMDb Rating'] - df['Your Rating']
 
 df['Title Type'] = df['Title Type'].apply(media_kind)
 
+#%%
 # Before adding to df, need to remove one-hot columns with very few values
 one_hot = df['Genres'].str.get_dummies(sep=', ')
+
+def get_frac(col):
+    if len(col.value_counts()) != 2:
+        return False
+    return col.value_counts()[1] / (col.value_counts()[0]+col.value_counts()[1])
+
+# See if the one-hot columns represent at least 20 percent of the columns
+def thres_check(col, thres=.2):
+    return get_frac(col) > thres
+
+to_drop = []
+for i in one_hot.columns:
+    if not thres_check(one_hot[i]):
+        to_drop.append(i)
+one_hot.drop(to_drop, axis=1, inplace=True)
+
+# Figure out a way to check other values count to drop
+# thres = .2
+# col = df['Title Type']
+# vals = list(col.value_counts().index)
+# counts = list(col.value_counts().values)
+# for i in range(len(counts)):
+#     print(vals[i], counts[i] / sum(counts))
+#     if (counts[i] / sum(counts)) < thres:
+#         print(col.values[i])
+
 df = pd.concat([df, one_hot], axis=1, sort=False)
 
+# Bin IMDb rating, year, Num Votes, Days waited to see, Diff in ratings
+
 # df.to_csv('ratings_clean.csv', index=False)
+
 # %%
 plt.scatter(df['IMDb Rating'], df['Your Rating'])
 plt.title('IMDb Rating vs. My Rating')
